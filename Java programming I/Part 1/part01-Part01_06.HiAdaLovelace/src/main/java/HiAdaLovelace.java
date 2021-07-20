6e5cee2e82e22d4a12a34a7cd1b35f0f5aec9683
shriram-s6 <86624773+shriram-s6@users.noreply.github.com>
2021-07-21 04:09:19 +0530

    Part 1

---
+++ Java programming I/Part 1/part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
@@ -0,0 +1,11 @@
+
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+        String hi = "Hi ";
+        
+        System.out.println(hi + name + "!");
+
+    }
+}
