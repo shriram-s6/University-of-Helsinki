5b4b785d87bb223b601205740d71ef232d53ce38
shriram-s6 <86624773+shriram-s6@users.noreply.github.com>
2021-07-21 04:06:15 +0530

    Part 1

---
+++ Java programming I/Part 1/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Write your program here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int num1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int num2 = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + ((double)(num1 + num2) / (double) 2));
+    }
+}
