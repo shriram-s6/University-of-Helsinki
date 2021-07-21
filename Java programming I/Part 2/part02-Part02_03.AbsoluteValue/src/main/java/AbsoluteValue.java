a4f49ac6bd1445bc7d9c599c527c8e5a3d685cb4
shriram-s6 <86624773+shriram-s6@users.noreply.github.com>
2021-07-21 05:52:19 +0530

    Part 2

---
+++ Java programming I/Part 2/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class AbsoluteValue {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.valueOf(scanner.nextLine());
+        System.out.print(Math.abs(number));
+
+    }
+}
