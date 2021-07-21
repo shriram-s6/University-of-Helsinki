bd960a8ed851c22076704039cf2f449674b1b340
shriram-s6 <86624773+shriram-s6@users.noreply.github.com>
2021-07-21 05:53:07 +0530

    Part 2

---
+++ Java programming I/Part 2/part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class SumOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int total = 0;
+        
+        while (true) {
+            
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                break;
+            }else {
+                total += number;
+            }
+        }
+        System.out.println("Sum of the numbers: " + total);
+    }
+}
